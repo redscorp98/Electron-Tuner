fa0a9a42cfef04d48957eded6455adadfbb6d31f
redscorp98 <sm_ja1@live.co.uk>
2020-06-23 17:55:18 +0100

    Remove commented code

--- renderer.js
+++ renderer.js
@@ -1,23 +1,15 @@
-let arr;
-let arrBuffer;
-let arrBuffer2;
 var audioCtx = new AudioContext;
-var analyser = audioCtx.createAnalyser();
 let recording;
-let execute = 0;
 const PERIOD = 500; // 500 ms
-let ctx;
-window.onload = function () { ctx = document.getElementById('myChart').getContext('2d'); }
-const chart = require("electron-chartjs")
 const Pitchfinder = require('pitchfinder')
+let mediaRecorder;
 main();
-
-
 
 async function setupInput() {
     return navigator.mediaDevices.getUserMedia({ audio: true })
         .then(device => {
-            return new MediaRecorder(device)
+            mediaRecorder = new MediaRecorder(device)
+            return true
         })
         .catch(error => {
             if (error.name === "NotFoundError") {
@@ -25,17 +17,9 @@ async function setupInput() {
             } else {
                 console.error(error)
             }
-            return null
+            return false
         })
-
 }
-
-let mediaRecorder;
-
-// setupInput().then(recorder => {
-//     mediaRecorder = recorder;
-// })
-
 
 async function record() {
     recording = window.setInterval(() => {
@@ -51,7 +35,7 @@ function stop() {
 }
 
 async function main() {
-    mediaRecorder = await setupInput();
+    await setupInput();
     mediaRecorder.ondataavailable = async function (e) {
         const blob = e.data
         const stream = blob.stream()
@@ -59,43 +43,12 @@ async function main() {
         reader.read().then(buffer => {
             audioCtx.decodeAudioData(buffer.value.buffer).then(audioBuffer => {
                 audioData = audioBuffer.getChannelData(0)
-                // fetchStream()
-                writeCSV(audioData)
-                // console.log(audioData)
-                // console.log("SaMPLE RATE: ", audioBuffer.sampleRate)
+                // writeCSV(audioData)
                 getFrequency(audioBuffer.sampleRate, audioData)
-                // play(audioBuffer);
             }).catch(e => {
                 console.log(e)
             })
         })
-
-        // const arr = await reader.read()
-        // arrBuffer = arr.value.buffer;
-        // console.log("Fetch ", arrBuffer)
-
-
-
-        // const response = await fetch(URL.createObjectURL(e.data))
-        // const arrBuffer2 = await response.arrayBuffer()
-        // const audioBuffer = await audioCtx.decodeAudioData(arrBuffer2)
-        // const audioData = audioBuffer.getChannelData(0)
-        /*         fetch(URL.createObjectURL(e.data)).then(response => {
-                    response.arrayBuffer().then(buffer => {
-                        console.log("in", buffer)
-                        audioCtx.decodeAudioData(buffer).then(audioBuffer => {
-                            audioData = audioBuffer.getChannelData(0)
-                            fetchStream()
-                            writeCSV(audioData)
-                            // console.log(audioData)
-                            // console.log("SaMPLE RATE: ", audioBuffer.sampleRate)
-                            getFrequency(audioBuffer.sampleRate, audioData)
-                            // play(audioBuffer);
-                        }).catch(e => {
-                            console.log(e)
-                        })
-                    })
-                }) */
     }
 }
 
@@ -106,16 +59,6 @@ function play(buf) {
     source.connect(audioCtx.destination);
     // Play immediately
     source.start(0);
-}
-
-async function fetchStream() {
-    if (blob) {
-        const stream = blob.stream()
-        const reader = stream.getReader()
-        const arr = await reader.read()
-        arrBuffer = arr.value.buffer;
-        console.log("Fetch ", arrBuffer)
-    }
 }
 
 async function getFrequency(sampleFrequency, byteArray) {
@@ -138,22 +81,4 @@ async function writeCSV(arr = []) {
     csvWriter.writeRecords(output).then(() => {
         console.log("Complete")
     })
-}
-
-function weee() {
-    const createCsvWriter = require('csv-writer').createArrayCsvWriter;
-    const csvWriter = createCsvWriter({
-        header: ['Index', 'Amplitude'],
-        path: 'file.csv'
-    });
-    const records = [
-        ['Bob', 'French, English'],
-        ['Mary', 'English']
-    ];
-
-    csvWriter.writeRecords(records)       // returns a promise
-        .then(() => {
-            console.log('...Done');
-        });
-
 }
