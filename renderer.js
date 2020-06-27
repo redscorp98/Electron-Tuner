280d34894e74549d2f27695833d38ec0f71e8126
redscorp98 <sm_ja1@live.co.uk>
2020-06-27 17:34:29 +0100

    Changed methods of listening to tone. Added HTML dropdown and events

--- renderer.js
+++ renderer.js
@@ -3,24 +3,88 @@ let recording;
 const PERIOD = 500; // 500 ms
 const Pitchfinder = require('pitchfinder')
 let mediaRecorder;
-main();
+let audioInputs;
 
-async function setupInput() {
-    return navigator.mediaDevices.getUserMedia({ audio: true })
-        .then(device => {
-            mediaRecorder = new MediaRecorder(device)
-            return true
-        })
-        .catch(error => {
-            if (error.name === "NotFoundError") {
-                console.error("ERROR. No audio input detected.")
-            } else {
-                console.error(error)
-            }
-            return false
-        })
+// When device list changes this is called
+navigator.mediaDevices.ondevicechange = function (event) {
+    updateInputList()
 }
 
+
+async function setupInput(options) {
+    if (audioInputs === null) {
+        console.log("Nothing to do")
+    }
+    else {
+        const device = (audioInputs.filter(input => input.label.includes(options.value)))
+        mediaDevice = await navigator.mediaDevices.getUserMedia({ audio: { deviceId: { exact: device.deviceId } } })
+        mediaRecorder = new MediaRecorder(mediaDevice)
+        mediaRecorder.ondataavailable = async function (e) {
+            const blob = e.data
+            const stream = blob.stream()
+            const reader = stream.getReader()
+            reader.read().then(buffer => {
+                audioCtx.decodeAudioData(buffer.value.buffer).then(audioBuffer => {
+                    audioData = audioBuffer.getChannelData(0)
+                    // writeCSV(audioData)
+                    getFrequency(audioBuffer.sampleRate, audioData)
+                }).catch(e => {
+                    console.log(e)
+                })
+            })
+        }
+        console.log(mediaRecorder)
+    }
+    // return navigator.mediaDevices.getUserMedia({ audio: true })
+    //     .then(device => {
+    //         mediaRecorder = new MediaRecorder(device)
+    //         return true
+    //     })
+    //     .catch(error => {
+    //         if (error.name === "NotFoundError") {
+    //             console.error("ERROR. No audio input detected.")
+    //         } else {
+    //             console.error(error)
+    //         }
+    //         return false
+    //     })
+}
+
+async function updateInputList() {
+    let select = document.getElementById("audio-inputs");
+    const inputs = await getMediaDevices()
+    audioInputs = inputs
+    if (inputs === null) {
+        console.log("No inputs to display")
+        return false
+    } else {
+        inputs.forEach(input => {
+            select.options[select.options.length] = new Option(input.label.slice(0, -12))
+        });
+        return true
+    }
+
+}
+
+
+async function getMediaDevices() {
+    let audioDevices = []
+    try {
+        const devices = await navigator.mediaDevices.enumerateDevices()
+        audioDevices = devices.filter(device => device.kind === "audioinput")
+        console.log(audioDevices)
+        // devices = await navigator.mediaDevices.getUserMedia({ audio: { deviceId: { exact: } } })
+    } catch (error) {
+        console.error(error)
+    }
+    return audioDevices
+}
+
+/* 
+    Record audio from microphone input
+    Record for PERIOD
+    Stop recording for 1000 - PERIOD
+ */
 async function record() {
     recording = window.setInterval(() => {
         mediaRecorder.start()
@@ -30,13 +94,18 @@ async function record() {
     }, 1000)
 }
 
+// Stop recording audio from the microphone input
 function stop() {
     clearInterval(recording)
 }
 
+/* 
+    Setup mediaRecorder interrupt and input
+*/
 async function main() {
-    await setupInput();
-    mediaRecorder.ondataavailable = async function (e) {
+    // await setupInput();
+    updateInputList()
+    /* mediaRecorder.ondataavailable = async function (e) {
         const blob = e.data
         const stream = blob.stream()
         const reader = stream.getReader()
@@ -49,9 +118,10 @@ async function main() {
                 console.log(e)
             })
         })
-    }
+    } */
 }
 
+// Play most recently recorded audio
 function play(buf) {
     var source = audioCtx.createBufferSource();
     source.buffer = buf;
@@ -61,12 +131,15 @@ function play(buf) {
     source.start(0);
 }
 
+// Calculate frequecny of most recently recorded audio buffer
 async function getFrequency(sampleFrequency, byteArray) {
     const detectPitch = Pitchfinder.YIN(sampleFrequency)
     const frequency = detectPitch(byteArray)
+    // Sometimes frequency is null
     console.log("Frequency: ", frequency)
 }
 
+// Writes CSV of recorded audio array
 async function writeCSV(arr = []) {
     const createCsvWriter = require('csv-writer').createArrayCsvWriter;
     const csvWriter = createCsvWriter({
@@ -81,4 +154,20 @@ async function writeCSV(arr = []) {
     csvWriter.writeRecords(output).then(() => {
         console.log("Complete")
     })
+}
+
+/*  Plays a tone of input frequency Hz
+    type of wave: sine, sqaure, sawtooth, triangle or custom (https://developer.mozilla.org/en-US/docs/Web/API/OscillatorNode/type)
+    length seconds
+ */
+function playTone(frequency = 330, type = "sine", length = 6) {
+    const oscillator = audioCtx.createOscillator()
+    oscillator.onended = function () {
+        console.log('Your tone has now stopped playing!');
+    };
+    oscillator.frequency.setValueAtTime(frequency, audioCtx.currentTime)
+    oscillator.type = type
+    oscillator.connect(audioCtx.destination)
+    oscillator.start()
+    oscillator.stop(audioCtx.currentTime + length);
 }
